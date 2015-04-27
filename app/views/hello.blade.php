2581d4b48aca1fea6acdcb384665c0c38790d60d
Svetlozar Kirkov <svetlozark@gmail.com>
2015-04-27 13:51:28 +0300

    created links for the latest threads and comments

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -17,12 +17,15 @@
     	echo "<h3>Latest Threads</h3>";
     	$latestThreads = DB::table('forum_threads')->orderBy('created_at', 'desc')->take(6)->get();
     	foreach ($latestThreads as $thr) {
+    		$thrLink = URL::route('forum-thread', $thr -> id);
     		echo "<div class=\"col-md-4\">";
+    		echo "<a href=\"$thrLink\" class=\"list-group-item homepage-latest-links hvr-fade\">";
     		echo "<b>$thr->title</b>" . "<br>";
     		$threadAuthor = DB::table('users')->where('id', $thr->author_id)->pluck('username');
     		echo "by: $threadAuthor <br>";
-    		$threadLastUpd = new DateTime($thr->updated_at);
-    		echo "last update: " . $threadLastUpd->format('d-m-Y H:i:s') . "<br>";
+    		$threadLastUpd = new DateTime($thr->created_at);
+    		echo "created: " . $threadLastUpd->format('d-m-Y H:i:s') . "<br>";
+    		echo "</a>";
     		echo "</div>";
     	}
     	echo "</div>";
@@ -31,12 +34,17 @@
     	echo "<h3>Latest Comments</h3>";
     	$latestComments = DB::table('forum_comments')->orderBy('created_at', 'desc')->take(6)->get();
     	foreach ($latestComments as $comm) {
+    		$thrLink = URL::route('forum-thread', $comm -> thread_id);
+    		$thrTitle = DB::table('forum_threads')->where('id', $comm -> thread_id)->pluck('title');
     		echo "<div class=\"col-md-4\">";
+    		echo "<a href=\"$thrLink\" class=\"list-group-item homepage-latest-links hvr-fade\">";
     		echo "<b>$comm->body</b>" . "<br>";
+    		echo "posted in: <u>$thrTitle</u> <br>";
     		$commAuthor = DB::table('users')->where('id', $comm->author_id)->pluck('username');
     		echo "by: $commAuthor <br>";
-    		$commLastUpd = new DateTime($comm->updated_at);
-    		echo "last edit: " . $commLastUpd->format('d-m-Y H:i:s') . "<br>";
+    		$commLastUpd = new DateTime($comm->created_at);
+    		echo "posted: " . $commLastUpd->format('d-m-Y H:i:s') . "<br>";
+    		echo "</a>";
     		echo "</div>";
     	}
     	echo "</div>";
