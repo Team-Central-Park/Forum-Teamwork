bbb599e2f48f48ce536ab0db2188eab3ed11abc2
Svetlozar Kirkov <svetlozark@gmail.com>
2015-04-28 01:24:14 +0300

    added background

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forum',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'svecloud_forum',
+			'username'  => 'svecloud_admin',
+			'password'  => '[8#[It,(b0s7',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
